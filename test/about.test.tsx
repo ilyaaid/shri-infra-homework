25f271b3575b53ca9f12923dea2246083cbeca80
Ilya Aleshin <idaleshin@mail.ru>
2025-07-13 16:32:44 +0300

    wrong test

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe('About', () => {
     it('Should render author name', () => {
         const { container } = render(<About />);
 
-        expect(container.textContent).toBe('AboutAuthor: Ilya');
+        expect(container.textContent).toBe('AboutAuthor: Ily');
     });
 });
